44d8ea11ad4228e00f1ae0263ab4ace2055a94f3
Tevon Talbert <TevonTalbert@hotmail.com>
2023-11-15 10:29:58 -0600

    adding forms

--- index.js
+++ index.js
@@ -28,4 +28,63 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
             .then(data => console.log(data))
     })
 
+// creating a node for the form
+const form = document.getElementById("form1");
+// creating a node for the image URL
+const DogPic = document.getElementById("image");
 
+//creating a submit event using the form node, also added error catching.
+form.addEventListener('submit', function(e) {
+ //prevent form from actually reloading the page or redirecting
+    e.preventDefault();
+// this node will hold the value of Dname when form is submitted
+    const Dawgname = document.getElementById("Dname").value
+// this node will hold the Pic URL value when submitted
+    const newpic = document.getElementById("Dogpic").value
+// this name will hold the Id key that will be returned when form is submitted
+    const DID = document.getElementById("DogID").value
+
+ // payload is our values from our form being created into an object and sent to the json 
+    const payload = {
+        "Dname": Dawgname, "Dogpic": newpic, "DogID": DID
+    };
+// Simple fetch 
+    fetch('http://localhost:3000/animals', {
+// our post 
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+// payload  is being stringify and converted to plain text
+        body: JSON.stringify(payload),
+            
+
+
+
+        
+    })
+        .then(res => res.json())
+        .then(data => {
+            var Dawgname = Dname;
+
+            console.log(payload);
+            
+
+
+         
+
+            
+
+
+
+ console.log(data);
+
+            
+
+        })
+// This process is successful if there is not console error
+        .catch(err => console.log(err));
+       
+
+
+})
