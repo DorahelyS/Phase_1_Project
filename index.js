c3b0e5d34323f10f74593728643a62c8afe59c11
Tevon Talbert <TevonTalbert@hotmail.com>
2023-11-16 14:13:45 -0600

    just for pull

--- index.js
+++ index.js
@@ -24,86 +24,104 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
             }
         })
             .then(res => res.json())
-        //             .then(animals => {
-        //                 animals.animals.forEach(animal => {
-        //                     // console.log(animal)
-        //                     renderAnimalInfo(animal)
-        //                     debugger
+                     .then(animals => {
+                         animals.animals.forEach(animal => {
+                             // console.log(animal)
+                             renderAnimalInfo(animal)
+                             console.log(animal);
 
-        //                 })
+                         })
 
-        //             })
-        //     })
+                     })
+             })
 
-        // function renderAnimalInfo(animal) {
-        //     const animalSpecies = document.createElement('li')
-        //     const animalAge = document.createElement('li')
-        //     const animalImage = document.createElement('img')
+function renderAnimalInfo(animal) {
 
-        //     animalImage.src = ""
-        //     animalAge.textContent = "Age: " + animal.age
-        //     animalSpecies.textContent = "Animal: " + animal.species
+    
 
-        //     animalList.append(animalSpecies, animalAge, animalImage)
-        //     debugger
-    }
-// creating a node for the form
-const form = document.getElementById("form1");
-// creating a node for the image URL
-const DogPic = document.getElementById("image");
 
-//creating a submit event using the form node, also added error catching.
-form.addEventListener('submit', function (e) {
-    //prevent form from actually reloading the page or redirecting
-    e.preventDefault();
-    // this node will hold the value of Dname when form is submitted
-    const Dawgname = document.getElementById("Dname").value
-    // this node will hold the Pic URL value when submitted
-    const newpic = document.getElementById("Dogpic").value
-    // this name will hold the Id key that will be returned when form is submitted
-    const DID = document.getElementById("DogID").value
+    const animalSpecies = document.createElement('li')
+    const animalAge = document.createElement('li')
+    const animalImage = document.createElement('img')
 
-    // payload is our values from our form being created into an object and sent to the json 
-    const payload = {
-        "Dname": Dawgname, "Dogpic": newpic, "DogID": DID
-    };
-    // Simple fetch 
-    fetch('http://localhost:3000/animals', {
-        // our post 
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        // payload  is being stringify and converted to plain text
-        body: JSON.stringify(payload),
+    
 
 
 
+    animalImage.src = dog.Url;
+            animalAge.textContent = "Age: " + animal.age
+             animalSpecies.textContent = "Animal: " + animal.species
 
+             animalList.append(animalSpecies, animalAge, animalImage)
 
-    })
-        .then(res => res.json())
-        .then(data => {
-            var Dawgname = Dname;
+        }
+                     
+    
+    
 
-            console.log(payload);
+        // creating a node for the form
+        const form = document.getElementById("form1");
+        // creating a node for the image URL
+        const DogPic = document.getElementById("image");
 
+        //creating a submit event using the form node, also added error catching.
+        form.addEventListener('submit', function (e) {
+            //prevent form from actually reloading the page or redirecting
+            e.preventDefault();
+            // this node will hold the value of Dname when form is submitted
+            const Dawgname = document.getElementById("Dname").value
+            // this node will hold the Pic URL value when submitted
+            const newpic = document.getElementById("Dogpic").value
+            // this name will hold the Id key that will be returned when form is submitted
+            const DID = document.getElementById("DogID").value
 
-
+            // payload is our values from our form being created into an object and sent to the json 
+            const payload = {
+                "Dname": Dawgname, "Dogpic": newpic, "DogID": DID
+            };
+            // Simple fetch 
+            fetch('http://localhost:3000/animals', {
+                // our post 
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                // payload  is being stringify and converted to plain text
+                body: JSON.stringify(payload),
 
 
 
 
 
+            })
+                .then(res => res.json())
+                .then(data => {
+                    var Dawgname = Dname;
 
-            console.log(data);
+                    console.log(payload);
+
+
+
+
+
+
+
+
+
+                    console.log(data);
+
+
+
+                })
+                // This process is successful if there is not console error
+                .catch(err => console.log(err));
 
 
 
         })
-        // This process is successful if there is not console error
-        .catch(err => console.log(err));
+const DB = fetch('http://localhost:3000/animals')
+    .then((resp) => resp.json())
+    .then((json) => (json))
+console.log(json);
 
 
-
-})
